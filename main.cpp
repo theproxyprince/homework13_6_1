282a9e07e9152e8b3387ebabb8efe44dfcf576bc
Ruslan <rrnanbarr@gmail.com>
2022-04-19 15:01:29 +0300

    fixed

--- main.cpp
+++ main.cpp
@@ -5,11 +5,11 @@ int main() {
     std::cout << "Введите размер вектора: ";
     int s;
     std::cin >> s;
-    std::vector<int> vec(s);
+    std::vector<int> vec1(s);
     for (int i = 0; i < s; i++) {
         int number;
         std::cin >> number;
-        vec[i] = number;
+        vec1[i] = number;
     }
 
     std::cout << "\nВведите число которое надо удалить: ";
@@ -17,18 +17,21 @@ int main() {
     std::cin >> num;
 
     std::cout << "До удаление: ";
-    for (int i : vec) {
+    for (int i : vec1) {
         std::cout << i << " ";
     }
 
-    for (auto iter = vec.begin(); iter != vec.end(); ++iter) {
-        if (*iter == num) {
-            vec.erase(iter);
+    // 2, 3, 4, 3, 5
+    std::vector<int> vec2;
+
+    for (int i = 0; i < vec1.size(); i++) {
+        if (vec1[i] != num) {
+            vec2.push_back(vec1[i]);
         }
     }
 
     std::cout << "\nПосле удаление: ";
-    for (int i : vec) {
+    for (int i : vec2) {
         std::cout << i << " ";
     }
 
